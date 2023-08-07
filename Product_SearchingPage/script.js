d845748ea5d1ea85a102881c6adbe2e68300f08f
Debasmita Sahoo <sdebasmita974@gmail.com>
2023-08-07 13:31:32 +0530

    Done with searching the product

--- Product_SearchingPage/script.js
+++ Product_SearchingPage/script.js
@@ -36,6 +36,7 @@
 (async () => {
     const url = 'https://fakestoreapi.com/products';
     const container = document.getElementById('container');
+    const searchinput=document.getElementById('SeacrchBar');
     const Product_result = async () => {
         try {
             const result = await fetch(url);
@@ -50,9 +51,9 @@
         return `<div class="list">
             <div class="product-img"><img src="${product.image}"></div>
             <div class="product-desc">
-              <h2>${product.title}</h2>
-              <p>${product.description}</p>
-              <button class="btn">${product.price}</button>
+              <h2>${product.title.split(" ").slice(0,10).join(" ")}</h2>
+              <p>${product.description.split(" ").slice(0,20).join(" ")}</p>
+              <button class="btn">$${product.price}</button>
             </div>
       </div>`
     };
@@ -64,5 +65,32 @@
         });
     }
     ShowProduct(Products);
+
+    const SearchContext=(existingtext,filterdtext)=>{
+        return existingtext.toString().toLowerCase().includes(filterdtext);
+    }
+    const search=(event)=>{
+        const searchtext=event.target.value.toLowerCase();
+        const filteredProducts=Products.filter((product)=>{
+            return (
+              SearchContext(product.title,searchtext)||
+              SearchContext(product.description,searchtext)||
+              SearchContext(product.price,searchtext)
+            );
+        });
+        if (filteredProducts.length===0) {
+            image=document.createElement('img');
+            image.src=`Images/text_not_found.gif`;
+            container.innerHTML="";
+            container.appendChild(image);
+        }
+        else{
+            ShowProduct(filteredProducts);
+
+        }
+        // ShowProduct(filteredProducts)
+    }
+    
+    searchinput.addEventListener('keyup',search);
 })()
 
